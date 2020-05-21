2c31dfd32b9819739bd271c78c6509af09332f2b
Yossi Adler <joisadler@gmail.com>
2020-05-21 15:34:35 +0300

    Yossi add List, GuidePreview, TrailPreview

---
+++ src/cmps/GuidePreview.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// const user = {
+//   "_id": "1234",
+//   "userName": "poiki123",
+//   "fullName": "shoka koko",
+//   "password": "1234",
+//   "isAdmin": false,
+//   "languages": ["he", "en"],
+//   "trails": [{ "mini-trail" }], // id, name, country,
+//   "imgUrl": "me.jpg"
+// }
+
+export const GuidePreview = props => {
+  const {
+    _id,
+    fullName,
+    imgUrl,
+  } = props.guide;
+  return (
+  <article className="guide-preview">
+    <Link to={ `/guide/${_id}` }>
+      <img className="guide-preview-image" src={ imgUrl } alt={ fullName }/>
+      <div className="guide-preview-info">
+        <h2 className="guide-preview-name">{ fullName }</h2>
+      </div>
+    </Link>
+  </article>);
+};
