cb860554dbb8d6e9cd3364f46a269408a83ca727
lior300 <lior9631@gmail.com>
2020-05-21 22:55:21 +0300

    lior

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import './style/global.scss';
-<<<<<<< HEAD
 import { Switch, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage'
 import { BookingPage } from './pages/BookingPage'
-=======
-import { Switch, Route} from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { Header } from './cmps/Header';
-import { Footer } from './cmps/Footer';
->>>>>>> c803b39cd2802a4ac83d67532b9a5da07bf119d4
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <BookingPage />
       {/* <NavBar /> */}
       {/* <Switch>
         <Route path="/" component={ HomePage } />
       </Switch> */}
-=======
-      {/* <Header /> */}
-      <Switch>
-        <Route path="/" component={ HomePage } />
-      </Switch>
-      {/* <Footer /> */}
->>>>>>> c803b39cd2802a4ac83d67532b9a5da07bf119d4
     </div>
   );
 }
