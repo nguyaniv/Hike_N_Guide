e4b154a52e09c18ecc8df6e2dc9a9931fc87ca43
Yossi Adler <joisadler@gmail.com>
2020-05-21 13:09:47 +0300

    trail preview changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './style/global.scss';
-import {  Switch, Route} from 'react-router';
-import {HomePage} from './pages/HomePage'
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       {/* <NavBar /> */}
-
       <Switch>
-
-      <Route path="/" component={HomePage} />
-
-
+        <Route path="/" component={ HomePage } />
       </Switch>
-        
-
-      <h2>test</h2>
     </div>
   );
 }
-
-
 
 export default App;
