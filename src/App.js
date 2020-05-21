92a650781c7cd6add71f4e182b4addd0ec418149
lior300 <lior9631@gmail.com>
2020-05-21 18:47:05 +0300

    lior

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './style/global.scss';
-import {  Switch, Route} from 'react-router-dom';
-import {HomePage} from './pages/HomePage'
+import { Switch, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage'
+import { BookingPage } from './pages/BookingPage'
 
 function App() {
   return (
     <div className="App">
+      <BookingPage />
       {/* <NavBar /> */}
-      <Switch>
+      {/* <Switch>
         <Route path="/" component={ HomePage } />
-      </Switch>
+      </Switch> */}
     </div>
   );
 }
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Rating from 'react-rating'
 
 class _BookingPage extends Component {
     state = {
-        user = {
+        user: {
             _id: "1234",
             userName: "poiki123",
             password: "1234",
             fullName: "shoka koko",
-            imgUrl: "",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.png",
+            rating: 3.5,
             isAdmin: false,
             languages: ["he", "en"],
             trails: [
@@ -20,7 +22,6 @@ class _BookingPage extends Component {
                 }
             ] // id, name, country
         }
-
     }
 
     render() {
@@ -28,8 +29,17 @@ class _BookingPage extends Component {
         return (
             <div className="booking-page">
                 <section>
-                    <img src={user.imgUrl} alt="" />
-                    <p>{user.userName}</p>
+                    <img className="booking-page-img" src={user.imgUrl} width="75px" />
+                    <p>{user.fullName}</p>
+                    <p>Rating:</p>
+                    <div>
+                        <Rating
+                            placeholderRating={user.rating}
+                            fullSymbol={'fa fa-star fa-2x medium'}
+
+                        />
+
+                    </div>
                 </section>
 
 
