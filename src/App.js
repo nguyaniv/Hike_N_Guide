0458a51ee304189872af683bab6457b81b7a1584
lior300 <lior9631@gmail.com>
2020-05-21 23:37:25 +0300

    gh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './style/global.scss';
-import { Switch, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage'
-import { BookingPage } from './pages/BookingPage'
-
+import {  Switch, Route} from 'react-router-dom';
+import {HomePage} from './pages/HomePage'
+import {TrailDetailsPage} from './pages/TrailDetailsPage'
+import {TrailsPage} from './pages/TrailsPage'
+import {Header} from './cmps/Header'
+import {Footer} from './cmps/Footer'
 function App() {
   return (
     <div className="App">
-      <BookingPage />
-      {/* <NavBar /> */}
-      {/* <Switch>
-        <Route path="/" component={ HomePage } />
-      </Switch> */}
+      {/* <Header /> */}
+      <Switch>
+        <Route path="/trail/:id" component={ TrailDetailsPage } />
+        <Route path="/trail" component={ TrailsPage } />
+        <Route exact path="/" component={ HomePage } />
+      </Switch>
+      {/* <Footer /> */}
     </div>
   );
 }
