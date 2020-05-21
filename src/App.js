669e092fe8bb02be83500875c74aac8ce101ad56
nguyaniv <nguyaniv@gmail.com>
2020-05-21 16:09:53 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './style/global.scss';
-<<<<<<< HEAD
 import {  Switch, Route} from 'react-router-dom';
 import {HomePage} from './pages/HomePage'
 import {TrailDetailsPage} from './pages/TrailDetailsPage'
 import {TrailsPage} from './pages/TrailsPage'
-=======
-import { Switch, Route} from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { Header } from './cmps/Header';
-import { Footer } from './cmps/Footer';
->>>>>>> b63de1f1295e9dff6c204a79e76ca0a33a0044c5
-
+import {Header} from './cmps/Header'
+import {Footer} from './cmps/Footer'
 function App() {
   return (
     <div className="App">
