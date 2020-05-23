247b9c95b0223e577c0db18fe149248014d95c9a
Yossi Adler <joisadler@gmail.com>
2020-05-23 20:15:43 +0300

    fix some eslint errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Router, Switch, Route, Link} from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import history from './history';
 import store from './store/store';
 
-
-
-
 ReactDOM.render(
-  <Provider store={store}>
-
-    <Router history={history}>
+  <Provider store={ store }>
+    <Router history={ history }>
       <App />
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
